ca9d19cd3ca8f3fee41a6264d6e5b7d745921e7c
Skiboreative <77804853+Skibocreative@users.noreply.github.com>
2021-10-04 15:07:06 -0700

    create repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
+function SecretComponent() {
+  return <h1>Secret information for authorized users only</h1>
+}
+
+function RegularComponent() {
+  <h1>Everyone can see this component.</h1>
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">Components here eventually</div>;
 }
 
 export default App;
+
+
+// <></> is fragment in place of <div></div>
